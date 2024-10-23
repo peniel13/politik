1db562b8e543f1c8beb4e5973eacab35def6246e
peniel <tosalabuzz@gmail.com>
2024-10-23 15:09:42 +0200

    hello

--- politik/politik/settings.py
+++ politik/politik/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#6-l)^v=*7nq@(qod=v97yi%em=rg04o!_au7dv$ea%216%nu_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["politik7.onrender.com", '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["politik.onrender.com", '127.0.0.1', 'localhost']
 
 
 # Application definition
